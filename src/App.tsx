6d6a3cb0eb0ca0ad9e7a92811e1386f46d41d020
Nathan Yang <nathanyang.w@outlook.com>
2026-06-18 21:01:25 +0200

    Add state for dice face generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // src/App.tsx
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const rollDice = (): void => {
-    console.log("Hello");
+  const [face, setFace] = useState(1);
+
+  const rollDice = () => {
+    setFace(Math.floor(Math.random() * 20) + 1);
   };
 
   return (
     <>
       <div>
         <h1>Dice Roller</h1>
-        <div id="dice20"></div>
+        <div id="dice20">Face: {face}</div>
         <button onClick={rollDice}>Roll</button>
       </div>
     </>
